4198b16e4cf250429c82263489adf9ce6106e826
Helton Furau <heltonnhandos@gmail.com>
2026-03-13 17:58:45 +0200

    feat: add Netlify configuration for build settings, environment variables, and custom headers to enhance deployment and security

--- project/next.config.js
+++ project/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['placehold.co', 'images.unsplash.com'],
+    domains: ['placehold.co', 'images.unsplash.com', 'images.pexels.com'],
   },
   async headers() {
     return [
